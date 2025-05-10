06f4beaec7f7701b07f2a19720d46d03fb4160a4
HousseinDhayne <dhayne_houssein@hotmail.com>
2025-05-09 21:13:07 -0400

    my first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello, World!")
+if __name__ == "__main__":
+    main()
+# This is a simple Python script that prints "Hello, World!" to the console.
+# It defines a main function that contains the print statement.
